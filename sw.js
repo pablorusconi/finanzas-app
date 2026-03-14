94b17f0d36f186c8e31d6fb6687d33da68cc1693
pablorusconi <100390009+pablorusconi@users.noreply.github.com>
2026-03-14 20:29:54 +1000

    Agrego PWA e Icono Vagonetas

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'finanzas-v1';
+const urlsToCache = [
+  '/finanzas-app/',
+  '/finanzas-app/index.html',
+  '/finanzas-app/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
