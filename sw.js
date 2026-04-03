7e612bfaf9c746d5b9cfacfb9fc5b92c99e98426
Pablo Rusconi <pabloerusconi@gmail.com>
2026-04-03 16:51:54 +1000

    fix: forzar actualizacion de cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finanzas-v2';
+const CACHE_NAME = 'finanzas-v3';
 const urlsToCache = [
   '/finanzas-app/',
   '/finanzas-app/index.html',
@@ -6,8 +6,17 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
